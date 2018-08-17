5edd820f17d270adaffe4d17c2cfd193637379a1
CodeMyst <codemyst@outlook.com>
2018-08-17 15:03:38 +0200

    Everything done

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # UnityDocumentationGenerator
 Generates the Unity's documentation (manual and scripting reference) into 2 json files for fast search.
+
+It's used for the [Brackeys Bot](https://github.com/YilianSource/brackeys-bot).
+
+Before running the app you have to get the offline Unity documentation and place `Manual` and `ScriptReference` folders into the Documentation folder (in the root). And be sure to remove the `docdata` folder from `Manual` and `ScriptReference` folders.
---
+++ source/app.d
@@ -0,0 +1,74 @@
+import std.file;
+import arsd.dom;
+import std.path;
+import std.json;
+import std.stdio;
+import std.array;
+import std.string;
+import std.algorithm;
+
+private enum Type
+{
+	Manual,
+	ScriptReference
+}
+
+void main ()
+{
+	DirIterator manualDir = dirEntries ("Documentation/Manual", SpanMode.shallow);
+	DirIterator scriptDir = dirEntries ("Documentation/ScriptReference", SpanMode.shallow);
+	File manualFile = File ("manual.json", "w");
+	File scriptFile = File ("script.json", "w");
+	string manualLink = "https://docs.unity3d.com/Manual/";
+	string scriptLink = "https://docs.unity3d.com/ScriptReference/";
+
+	JSONValue manual = generate (manualDir, manualLink, Type.Manual);
+	JSONValue scriptReference = generate (scriptDir, scriptLink, Type.ScriptReference);
+
+	manualFile.writeln (manual.toString);
+	scriptFile.writeln (scriptReference.toString);
+}
+
+private JSONValue generate (DirIterator dir, string baseLink, Type type)
+{
+	JSONValue res;
+	JSONValue [] entries;
+	foreach (DirEntry m; dir)
+	{
+		string contents = cast (string) read (m.name);
+		Document doc = new Document (contents);
+		if (doc.title.length <= 14)
+			continue;
+
+		const string link = format ("%s%s", baseLink, baseName (m.name));
+		string title = "";
+
+		final switch (type)
+		{
+			case Type.Manual:
+				title = stripLeft (doc.title [16..$]);
+				break;
+			case Type.ScriptReference:
+				title = stripLeft (doc.title [23..$]);
+				break;
+		}
+
+		auto fullDescription = doc.getElementsByTagName ("p") [0].innerText;
+
+		const long indexOfDot = indexOf (fullDescription, '.') + 1;
+		string description = "";
+		if (indexOfDot >= 1)
+			description = fullDescription [0..indexOfDot];
+		else
+			description = fullDescription;
+
+		JSONValue json = [ "link": link ];
+		json.object ["title"] = JSONValue (title);
+		json.object ["description"] = JSONValue (description);
+
+		entries ~= json;
+	}
+
+	res = entries;
+	return res;
+}
